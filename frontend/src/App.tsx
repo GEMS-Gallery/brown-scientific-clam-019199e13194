8e6458c78ef31676b5b308dd4ee931d8049cbed4
nate gurian <nategurian@gmail.com>
2024-08-28 12:51:22 -0400

    019199e5-d663-7553-88c0-07412032e08b

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Internet Computer App</h1>
+      <h1>Retro Internet Computer App</h1>
       <button onClick={handleGreet}>Greet</button>
       {greeting && <p>{greeting}</p>}
     </div>
